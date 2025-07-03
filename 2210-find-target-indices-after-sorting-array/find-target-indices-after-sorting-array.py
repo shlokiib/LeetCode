d129c56b5f38cf5736a0700e64acec5be79bfaf7
Shlok Bhalmey <88663499+shlokiib@users.noreply.github.com>
2025-07-03 12:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.17%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -2,17 +2,17 @@ from typing import List
 
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        # First pass: count how many elements are less than target
         less = 0
+        count = 0
+        arr = []
+
         for num in nums:
             if num < target:
                 less += 1
-        
-        # Second pass: count how many are equal to target and yield indices
-        result = []
-        for num in nums:
-            if num == target:
-                result.append(less)
-                less += 1  # index increases for each target occurrence
-                
-        return result
+            elif num == target:
+                count += 1
+
+        for i in range(less, less + count):
+            arr.append(i)
+
+        return arr
